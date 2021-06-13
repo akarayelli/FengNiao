ee5cddd213ee13bdfc87601729e5c430b8fa9d0e
Alican Karayelli <alicankarayelli@gmail.com>
2021-06-13 15:32:29 +0300

    install sh file refactored.

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 swift package clean
-swift test
 swift build -c release
-cp .build/release/FengNiao /usr/local/bin/fengniao
+cp .build/release/FengNiao /usr/local/bin/ifengniao
+
+export FENGNIAO_OUTPUT=$(ifengniao --exclude Carthage Pods)
+
+
+echo "#################################################"
+
+
+if [[ $FENGNIAO_OUTPUT = 0* ]] 
+then 
+ echo Gereksiz dosya bulunmuyor 🎉 
+ else 
+
+  echo $FENGNIAO_OUTPUT | envman add --key UNUSED_ASSET_MESSAGE
+  
+  envman run bash -c 'echo $UNUSED_ASSET_MESSAGE'
+
+ fi
+
+echo "#################################################"
+
+
+
